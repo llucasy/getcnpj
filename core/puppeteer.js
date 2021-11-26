c718cf0832a6e460cfdb9f7b8ae7ea0a0b2b8d43
Lucas ROdrigues <llucasy@hotmail.com>
2021-11-26 18:07:04 -0300

    First Commit

---
+++ core/puppeteer.js
@@ -0,0 +1,65 @@
+import puppeteer from 'puppeteer-core'
+import chrome from 'chrome-aws-lambda'
+
+export async function getOptions() {
+  const isDev = !process.env.AWS_REGION
+  let options;
+
+  const chromeExecPaths = {
+    win32: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    linux: '/usr/bin/google-chrome',
+    darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  }
+
+  const exePath = chromeExecPaths[process.platform]
+
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+      defaultViewport: null
+    }
+  } else {
+    options = {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless
+    }
+  }
+
+  return options
+}
+
+let _page
+async function getPage() {
+  if (_page) {
+    return _page
+  }
+
+  const options = await getOptions()
+  const browser = await puppeteer.launch(options)
+
+  _page = await browser.newPage()
+
+  return _page
+}
+
+export async function getCNPJ(cidade) {
+  const page = await getPage();
+
+  await page.goto(`https://cnpj.biz/empresas/${cidade}`, { waitUntil: "networkidle2" });
+  const links = await page.evaluate(() => {
+    const arr = []
+    document.querySelectorAll('.hero a').forEach((e, i, a) => {
+      arr.push(a[i].href)
+    })
+    const cnpjs = arr.filter((e, i) => arr.indexOf(e) === i);
+    const nextPage = cnpjs.pop()
+
+    return { cnpjs, nextPage }
+  })
+
+  return links
+
+}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getCNPJ } from "../../core/puppeteer"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+
+  const cidade = req.query.cidade || 'sao-paulo-sp'
+
+  const links = await getCNPJ(cidade)
+  res.status(200).json(links)
 }
